1f598a50de10ea312bcef18354cbbf49c751bb39
divyansh0505 <divyanshsachdeva198@gmail.com>
2025-07-06 22:43:51 +0530

    removed some comments

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.title("🔍 Personalized Job Recommendation System")
 uploaded_file = st.file_uploader("Upload your resume", type=["pdf", "docx"])
 if uploaded_file:
     st.success("Resume uploaded successfully!")
-    # You'll extract text later
+    
